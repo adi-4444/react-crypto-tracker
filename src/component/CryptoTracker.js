9b1d430a8cadfa543605fad3c3d77bcb4421bc65
Adi <adinarayana4444@gmail.com>
2022-07-19 23:55:33 +0530

    integrated Api

--- src/component/CryptoTracker.js
+++ src/component/CryptoTracker.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import axios from 'axios'
 import './CryptoTracker.css'
 
 const CryptoTracker = () => {
+
+   const [data, setData] = useState("")
+
+   useEffect(() => {
+      axios.get("https://api.coinstats.app/public/v1/coins?skip=0&currency=USD")
+      .then (res => setData(res.data.coins))
+      console.log(data)
+   },[])
+
+
+
+
   return (
     <div>
-
       <div className='search-container'>
          <h1>Crypto Tracker</h1>
-         <div>
-            <input className='search-bar' type={"text"} placeholder={"Search CryptoCurrency"}/>
-            <button className='search-btn'>Search</button>
-         </div>
+         <input className='search-bar' type={"text"} placeholder={"Search CryptoCurrency"}/>
       </div>
 
-      <DataCard />
+         {
+            data.length>0 && 
+            <>
+               {data.map(crypto => 
+                  <DataCard key={crypto.id} name={crypto.name} rank={crypto.rank} price={crypto.price}
+                  marketcap={crypto.marketCap}
+                  />
+                  )}
+            </>
+         }
+      
 
     </div>
   )
@@ -23,9 +42,9 @@ export default CryptoTracker;
 
 
 
-export const DataCard = () => {
+export const DataCard = ({name,rank,price,marketcap}) => {
   return (
-    <div>
+    <div className='crypto-wrap'>
          <div className='crypto-card'>
 
              <div className='card-img'>
@@ -34,12 +53,12 @@ export const DataCard = () => {
 
              <div className='card-data'>
                   <div className='crypto-name'>
-                     <p>Rank :</p>
-                     <h2>Bitcoin</h2>
+                     <p>Rank : {rank}</p>
+                     <h2>{name}</h2>
                   </div>
                   <div className='crypto-data'>
-                    <p>Price in $ :</p>
-                    <p>MarkatCap : </p>
+                    <p>Price in : ${price}</p>
+                    <p>MarkatCap : {marketcap} </p>
                   </div>
                 
                 <button className='btn-link'>Go to Website</button>
